82e3cf416f48183ed5b80f54e196cf874710542d
Ankur Kumar <ak6550352@gmail.com>
2024-11-17 02:34:49 +0530

    update json limit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import path from 'path'
 const app = express();
-app.use(express.json({ limit: "16kb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.static("public"));
 app.use('/api/docs',express.static(path.join(__dirname, 'public')))
 app.use(
